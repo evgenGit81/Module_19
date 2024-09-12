5edc106ceac109450c2ed835fd611879916c6984
evgen81Git <evgeniynrtv@gmail.com>
2024-09-13 01:21:29 +0700

    1

--- DBdjangoDB/DBdjangoDB/task1/models.py
+++ DBdjangoDB/DBdjangoDB/task1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10000000, decimal_places=2)
+    age = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name #Вывод по умолчанию, здесь только поле name. Остальные (balance, age) не выводятся
+
+class Game(models.Model):
+    title = models.CharField(max_length=500)
+    cost = models.DecimalField(max_digits=1000000, decimal_places=2)
+    size = models.DecimalField(max_digits=1000000000000, decimal_places=5)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, symmetrical=False)
+
+    def __str__(self):
+        return self.title
+
